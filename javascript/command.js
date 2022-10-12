e5392d4a4c31b74bf149d0d2e03de9ccbadf9be9
SaraswatAnushka <saraswatanushka007@gmail.com>
2022-10-12 06:07:55 +0530

    almost

--- javascript/command.js
+++ javascript/command.js
@@ -1,4 +1,4 @@
-function clearErrors(){
+/*function clearErrors(){
     errors = document.getElementsByClassName("formerror");
     for(let item of errors)
         {
@@ -28,24 +28,66 @@ function validateForm()
         seterror("phone","*Invalid Phone number");
         returnVal=false;
     }
+    
+    return returnVal;
+
+}*/
+function clearErrors() {
+    errors = document.getElementsByClassName("formerror");
+    for (let item of errors) {
+        item.innerHTML = ""
+    }
+}
+function seterror(id, error) {
+    element = document.getElementById(id);
+    element.getElementsByClassName("formerror")[0].innerHTML = error;
+}
+
+function validateForm() {
+    var returnVal = true;
+    var kudos = document.forms["myForm"]["fname"].value;
+    if (kudos.length < 3) {
+        seterror("name", "*Length of name is too short");
+        returnVal = false;
+    }
+
+    var phone = document.forms["myForm"]["fphone"].value;
+
+    if (phone.length != 10 || /^[0-9]*$/.test(phone) == false) {
+        seterror("phone", "*Invalid phone number");
+        returnVal = false;
+    }
+
+    var email = document.forms["myForm"]["fmail"].value;
+    let atpos = email.indexOf("@");
+    let dotpos = email.indexOf(".");
+    console.log(atpos);
+    console.log(dotpos);
+    if (dotpos - atpos < 2) {
+        seterror("email", "*Enter a valid email address");
+        returnVal = false;
+    }
 
     var pass = document.forms["myForm"]["fpass"].value;
+
+    if (/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*]).{8,20}$/.test(pass) == false) {
+        seterror("pass", "*invalid");
+        return returnVal = false;
+    }
+
+
     var cpass = document.forms["myForm"]["fcpass"].value;
-    if(pass.length < 8)
-    {
-        seterror("pass","*Length of password should be atleast 8 characters long");
-        returnVal=false;
+
+    if (/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*]).{8,20}$/.test(pass) == false) {
+        seterror("cpass", "*invalid cpass");
+        return returnVal = false;
     }
-    if(cpass.length < 8 )
-    {
-        seterror("cpass","*Length of password should be atleast 8 characters long");
-        returnVal=false;
-    }
-    if(pass != cpass)
-    {
-        seterror("cpass","*Passwords dont match");
-        returnVal=false;
-    }
+
+
+
+
+
+
     return returnVal;
 
 }
